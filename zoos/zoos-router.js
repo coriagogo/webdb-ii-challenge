ce56e8041a4924439dee115310ba77a98fbfa86b
coriagogo <pkcparis@gmail.com>
2019-06-04 16:06:26 -0400

    update and delete added

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -35,7 +35,7 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
     db('zoos')
-    .insert(req.body, 'id')
+        .insert(req.body, 'id')
         .then(ids => {
             res.status(201).json(ids);
         })
@@ -43,5 +43,40 @@ router.post('/', (req, res) => {
             res.status(500).json(error);
         })
 });
+
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+    db('zoos')
+        .where({ id: req.params.id })
+        .update(changes)
+        .then(count => {
+            if(count > 0){
+                const unit = count > 1 ? 'records' : 'record';
+                res.status(200).json({ message: `${count} ${unit} updated` })
+            } else {
+                res.status(404),json({ message: 'Zoo not found' })
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    db('zoos')
+        .where({ id: req.params.id })
+        .delete()
+        .then(count => {
+            if(count > 0){
+                const unit = count > 1 ? 'records' : 'record';
+                res.status(200).json({ message: `${count} ${unit} deleted` })
+            } else {
+                res.status(404),json({ message: 'Zoo not found' })
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+})
 
 module.exports = router;
